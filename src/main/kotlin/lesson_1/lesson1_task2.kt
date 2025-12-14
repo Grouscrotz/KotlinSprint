c354e5365a71b47416d2afae9a0560f5b526e679
Semyon <semyonkarabanov@gmail.com>
2025-12-14 09:03:45 +0300

    KS-1-2 solving task

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thanksMessage: String = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(thanksMessage)
+
+    var numberOfEmployees: Int = 2000
+    // println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
